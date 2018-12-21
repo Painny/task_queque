460b5a1ff737fdb1636cbb08055605abd3f886dd
pengyu <917103429@qq.com>
2018-12-21 12:34:36 +0800

    测试丢任务

--- src/master.php
+++ src/master.php
@@ -13,62 +13,114 @@ class Master{
     //当前子进程数量
     private $child_num;
 
-    //空闲子进程数量
-    private $free_child_num;
-
     //子进程pid数组
     private $child_pid;
 
     //最大子进程数量
     private $max_child_num;
 
-    //最小子进程数量
-    private $min_child_num;
-
     //任务检测间隔时间(秒)
     private $task_check_time;
 
-    public function __construct($name,$max_child_num=3,$min_child_num=1,$task_check_time=5)
+    //redis连接实例
+    private $redis;
+
+    public function __construct($name,$max_child_num=3,$task_check_time=5)
     {
         $this->name=$name;
         $this->max_child_num=$max_child_num;
-        $this->min_child_num=$min_child_num;
+
         $this->task_check_time=$task_check_time;
     }
 
     public function run(){
         //设置进程名
         cli_set_process_title($this->name);
+        //连接redis
+        $this->connectRedis();
 
-        //模拟任务
-        $task=["a","b","c"];
-
+        //todo 模拟丢任务
+        $this->addTask();
+        
         //开始任务检测
         while (true){
+            $taskData=$this->checkTask();
 
-            $tmp=array_pop($task);
-
-            if($tmp){
-                $this->doTask($tmp);
+            if($taskData){
+                new Worker($this->name."_worker",$taskData);
             }
 
             sleep($this->task_check_time);
         }
     }
 
-    private function doTask($task)
+    //模拟丢任务
+    private function addTask()
     {
-        $pid=pcntl_fork();
-        //子进程，执行任务
-        if($pid == 0){
-            cli_set_process_title($this->name."_worker");
-            echo $task.PHP_EOL;
-            sleep(60);
-            exit();
-        }else{  //父进程,记录子进程信息
-            $this->child_num++;
-            $this->child_pid[]=$pid;
+        for($i=0;$i<3;$i++){
+            $this->redis->lPush(config("task","list"),"make_pay_code");
+            $data=array(
+                "flag"          =>  0,
+                "code_list_key" =>  "pay_code_list_{$i}"
+            );
+            $this->redis->lPush("make_pay_code",json_encode($data));
         }
+
     }
+
+    //连接redis
+    private function connectRedis()
+    {
+        $redis=new Redis();
+        $redis->connect(
+            config("redis","host"),
+            config("redis","port")
+        );
+        $redis->auth(config("redis","passwd"));
+        $redis->select(config("redis","db"));
+        $this->redis=$redis;
+    }
+
+    //检测任务
+    private function checkTask()
+    {
+        //检测redis是否断线
+        if($this->redis->ping()!=="+PONG"){
+            $this->connectRedis();
+        }
+
+        //从所有任务类型列表中获取可以执行的任务类型
+        $taskType=$this->redis->rPop(config("task","list"));
+        //无任务
+        if(!$taskType){
+            return null;
+        }
+
+        //无效任务类型
+        if(!in_array($taskType,config("task","type"))){
+            return null;
+        }
+
+        //从该类型的任务列表中取除具体任务数据
+        $taskData=$this->redis->rPop($taskType);
+        if(!$taskData){
+            return null;
+        }
+
+        return $taskData;
+        /*
+         * 生成学生付款码任务数据：
+         *  {
+         *      "flag":0|1|2,                               生成文件包含内容，0二维码，1文本，2两者
+         *      "code_list_key":"pay_code_list_xxx"         存付款码列表的redis key
+         *  }
+         *
+         *
+         *
+         */
+
+    }
+
+
 
 }
