ee86e4b5910413717e8a5d4ead0e5675b151d707
pengyu <917103429@qq.com>
2018-12-21 10:26:54 +0800

    示例配置

--- src/config-exp.php
+++ src/config-exp.php
@@ -2,6 +2,38 @@
 /**
  * Created by PhpStorm.
  * User: pengyu
- * Date: 2018/12/21
- * Time: 10:18
+ * Date: 2018/12/20
+ * Time: 17:57
  */
+
+return array(
+
+    "redis" =>  array(
+        "host"  =>  "127.0.0.1",
+        "port"  =>  "6379",
+        "passwd"=>  ""
+    ),
+
+    "mysql" =>  array(
+        "host"  =>  "127.0.0.1",
+        "port"  =>  "3306",
+        "db"    =>  "db",
+        "user"  =>  "root",
+        "passwd"=>  "root"
+    ),
+
+    'mail'  =>  array(
+        "driver"    =>  "smtp",
+        "host"      =>  "smtp.exmail.qq.com",
+        "port"      =>  465,
+        "username"  =>  "service@ervice.com",
+        "passwd"    =>  "",
+        "encryption"=>  "ssl"
+    ),
+
+    "log"   =>  array(
+        "path"  =>  realpath(dirname(__DIR__)).DIRECTORY_SEPARATOR."log",
+        "file"  =>  "worklog.log",
+        "fmt"   =>  "[time] [type] [msg] [code]:in [line] line"
+    )
+);
