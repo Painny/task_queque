f20683d4e6563e58bbd17d451da85253e77184b0
pengyu <917103429@qq.com>
2018-12-27 16:33:24 +0800

    test

--- src/master.php
+++ src/master.php
@@ -35,7 +35,7 @@ class Master{
 
     public function run(){
         declare(ticks = 1);
-        pcntl_signal(SIGCHLD,"childExit");
+        var_dump(pcntl_signal(SIGCHLD,"childExit"));
         //设置进程名
         cli_set_process_title($this->name);
         //连接redis
