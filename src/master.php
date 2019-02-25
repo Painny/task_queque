53f767217cc1513dbcad639e41d6fab203f7ba8c
pengyu <917103429@qq.com>
2019-02-25 16:24:50 +0800

    status输出带颜色信息

--- src/master.php
+++ src/master.php
@@ -339,19 +339,20 @@ class Master{
     //运行状态信息
     private function status()
     {
-        if(!$this->isRunning()){
-            return "system is <r>stoped<\r>\n";
-        }
         //linux颜色输出对应值
         $red = "\033[31m";
         $green = "\033[40m";
         $end = "\033[0m";
 
-        //守护进程pid
-        $pid=$this->getPid();
+        if(!$this->isRunning()){
+            $info="system is <r>stoped<\r>\n";
+        }else{
+            //守护进程pid
+            $pid=$this->getPid();
+            $info="main process is <g>running</g>,the pid file is {$this->pidFile},pid is {$pid}\n";
+        }
 
-        $info="main process is <g>running</g>,the pid file is {$this->pidFile},pid is {$pid}\n";
-        $info=str_replace(array("<r>","<g>","</r>","</g?"),array($red,$green,$end,$end),$info);
+        $info=str_replace(array("<r>","<g>","</r>","</g>"),array($red,$green,$end,$end),$info);
         return $info;
     }
 
